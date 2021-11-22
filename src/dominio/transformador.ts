2f2d0d8c521f7fc9b0a7697eb423f91d375e8dd6
agustin.aguilera424 <agustina@epidataconsulting.com>
2021-11-21 22:47:54 -0300

    Etapa 3 - 1 - Transformador

---
+++ src/dominio/transformador.ts
@@ -0,0 +1,51 @@
+import { Country, RestCountriesAPI } from '../api/rest_countries_api';
+import { Pais } from './pais';
+
+export class Transformador {
+    
+    public countries: Country[];
+
+    constructor( countries :Country[]) {
+        this.countries = countries;
+    }
+
+    public countriesApais() {
+        return this.countries.map( async(country: Country) => { 
+            const limitrofe = await this.getLimitrofe(country);
+            return new Pais(
+                country.name,
+                country.alpha3Code,
+                country.population,
+                country.area || country.population,
+                country.region,
+                country.currencies[0]? country.currencies[0].code : 'USD',
+                0, // No se cua es cotizacionDolar de la api
+                limitrofe,
+                country.regionalBlocs.map( (bloc) => bloc.name ),
+                country.languages.map( (language) => language.name )
+            )
+        } )
+    }
+
+    private async getLimitrofe(country: Country) {
+        const api =  new RestCountriesAPI();
+        const data = await api.buscarPaisesPorNombre(country.name);
+        return data.map( (country: Country) => this.countryApais(country) ); 
+    }
+
+    public countryApais(country: Country) {
+        return new Pais(
+            country.name,
+            country.alpha3Code,
+            country.population,
+            country.area || country.population,
+            country.region,
+            country.currencies[0]? country.currencies[0].code : 'USD',
+            0, // No se cua es cotizacionDolar de la api
+            [],
+            country.regionalBlocs.map( (bloc) => bloc.name ),
+            country.languages.map( (language) => language.name )
+        )
+    }
+    
+}

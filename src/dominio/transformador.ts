157b34b5f07ef82a76b24fc8bc83f7ce397a6a88
agustin.aguilera424 <agustina@epidataconsulting.com>
2021-11-22 21:35:57 -0300

    Llamada a la api de conversión

--- src/dominio/transformador.ts
+++ src/dominio/transformador.ts
@@ -1,17 +1,22 @@
 import { Country, RestCountriesAPI } from '../api/rest_countries_api';
 import { Pais } from './pais';
+import { CurrencyConverterAPI } from '../api/currency_converter_api';
 
 export class Transformador {
     
     public countries: Country[];
+    public currencyApi : CurrencyConverterAPI;
 
     constructor( countries :Country[]) {
         this.countries = countries;
+        this.currencyApi = new CurrencyConverterAPI('d956ad8ad88fce288555');
+        
     }
 
     public countriesApaises() {
         return this.countries.map( async(country: Country) => { 
             const limitrofe = await Promise.all(this.getLimitrofe(country));
+            const cotizacion = await this.currencyApi.convertirDolarA(country.currencies[0]? country.currencies[0].code : 'USD');
             return new Pais(
                 country.name,
                 country.alpha3Code,
@@ -19,7 +24,7 @@ export class Transformador {
                 country.area || country.population,
                 country.region,
                 country.currencies[0]? country.currencies[0].code : 'USD',
-                0, // No se cua es cotizacionDolar de la api
+                cotizacion, 
                 limitrofe,
                 country.regionalBlocs?.map( (bloc) => bloc.name ),
                 country.languages?.map( (language) => language.name )
@@ -32,13 +37,16 @@ export class Transformador {
         const limitrofe = country.borders.map( async(code: string) => {
             return await api.paisConCodigo(code);
         });
-        return limitrofe.map( (limitrofe) => {
+        return limitrofe.map( async (limitrofe) => {
+            const cotizacion = await this.currencyApi.convertirDolarA((await limitrofe).currencies[0]? (await limitrofe).currencies[0].code : 'USD');
             return limitrofe.then( (country) => {
-                return this.countryApais(country);});
+                return this.countryApais(country, cotizacion);});
         });
     }
 
-    public countryApais(country: Country) {
+    public countryApais(country: Country, cotizacion: number) {
+      
+    
         return new Pais(
             country.name,
             country.alpha3Code,
@@ -46,7 +54,7 @@ export class Transformador {
             country.area? country.area : country.population,
             country.region,
             country.currencies[0]? country.currencies[0].code : 'USD',
-            0, // No se cua es cotizacionDolar de la api
+            cotizacion,
             [],
             country.regionalBlocs?.map( (bloc) => bloc.name ),
             country.languages?.map( (language) => language.name )

655537f19df748078fd85dcbff67843cd102da13
Agustin Aguilera <agustina@epidataconsulting.com>
2021-11-25 15:20:36 -0300

    Metodo para cargar los datos desde la api

--- src/dominio/observatorio.ts
+++ src/dominio/observatorio.ts
@@ -1,11 +1,22 @@
 import R from 'ramda'
 import { Pais } from './pais';
+import { RestCountriesAPI } from '../api/rest_countries_api';
+import { Transformador } from './transformador';
 export class Observatorio {
 
     public paises: Pais[];
+    public api: RestCountriesAPI
 
-    constructor(paises: Pais[]) {
-        this.paises = paises;
+    constructor(paises?: Pais[]) {
+        this.api = new RestCountriesAPI();
+        this.paises = paises || []
+    }
+
+    public async cargarDatosDeApi() {
+        const apiData = await this.api.todosLosPaises();
+        const transformador = await new Transformador(apiData); 
+        const data = await Promise.all(await transformador.countriesApaises());
+        this.paises = data;
     }
 
     private obtenerPorNombre(nombreDelPais: string): Pais {
@@ -16,6 +27,7 @@ export class Observatorio {
     public sonLimitrofes(nombrePrimerPais: string, nombreSegundoPais: string): boolean {
         const primerPais = this.obtenerPorNombre(nombrePrimerPais);
         const segundoPais = this.obtenerPorNombre(nombreSegundoPais);
+        console.log({miPais: this.paises})
         return primerPais.esLimitrofeDe(segundoPais);
     }
 

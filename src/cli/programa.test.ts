3b48a7660434179b1d9c8d6e6fed29ff18147ecf
Federico Aloi <federico.aloi@gmail.com>
2021-12-03 17:39:25 -0300

    Test de ejemplo para el programa

--- src/cli/programa.test.ts
+++ src/cli/programa.test.ts
@@ -1,6 +1,7 @@
 import { mocked } from "ts-jest/utils";
 import { consola } from "./consola";
 import programa from "./programa";
+import { RestCountriesAPIMock } from "../mocks/rest_countries_api";
 
 // Le pedimos a Jest que "imposte" el módulo completo
 jest.mock("./consola");
@@ -9,14 +10,28 @@ jest.mock("./consola");
 // Esto "le agrega" al objeto tenga los métodos de Jest para interactuar con mocks
 const consolaMock = mocked(consola);
 
-// TODO: agregar mock para la RestCountries API
-
 describe("Programa", () => {
-  //it("buscar país", async () => {
-  //consolaMock.leer.mockReturnValue("2.1");
-  //await programa.ejecutar();
-  //expect(consola.escribir).toHaveBeenCalledWith(
-  //"Ingresa el nombre del pais:"
-  //);
-  //});
+  it("devuelve el continente más plurinacional", async () => {
+    const apiMock = new RestCountriesAPIMock();
+    programa.api = apiMock;
+
+    consolaMock.leer.mockReturnValueOnce("3.2").mockReturnValueOnce("4");
+    await programa.ejecutar();
+    expect(consola.escribir).toHaveBeenCalledWith("Americas");
+  });
+
+  it("devuelve el continente más plurinacional", async () => {
+    const apiMock = new RestCountriesAPIMock();
+    programa.api = apiMock;
+
+    consolaMock.leer
+      .mockReturnValueOnce("1.1")
+      .mockReturnValueOnce("Paraguay")
+      .mockReturnValueOnce("4");
+
+    await programa.ejecutar();
+
+    // TODO: corregir el fixture
+    expect(consola.escribir).toHaveBeenCalledWith("No");
+  });
 });
